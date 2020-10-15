cda68fead1f50d7b3f67b92f390ab8df01897f24
John Dorman <namrods15@gmail.com>
2020-10-15 17:40:45 +0200

    updated for part 3

---
+++ client/src/components/layouts/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <MainContainer>
+      <h1>
+        Welcome to my <br /> MERN-Stack Blog
+      </h1>
+    </MainContainer>
+  );
+};
+
+export default Header;
+
+// Main Container
+const MainContainer = styled.header`
+  background: url(../../images/header-bg.jpg) no-repeat center/cover;
+  height: 25rem;
+
+  h1 {
+    transform: translate(-50%, -50%);
+    color: white;
+    font-weight: 900;
+    position: absolute;
+    top: 25%;
+    left: 50%;
+  }
+`;
